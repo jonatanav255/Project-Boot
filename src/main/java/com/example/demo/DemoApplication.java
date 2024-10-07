0ca2039d05074df27eb75f32c7664e8219d05df2
Jonatan <jonatanav255@hotmail.com>
2024-10-07 17:37:39 -0600

    playing with beans

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,19 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+      ApplicationContext context =  SpringApplication.run(DemoApplication.class, args);
+
+      MyFirstClass myFirstClass = context.getBean(MyFirstClass.class);
+
+      String message = myFirstClass.HelloWorld();
+      System.out.println(message);
+
+    }
 
 }
---
+++ src/main/java/com/example/demo/MyFirstClass.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyFirstClass {
+
+    public String HelloWorld() {
+        return "Hello from the first class";
+    }
+}
